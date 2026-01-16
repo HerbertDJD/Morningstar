914d77a0219406339cc24788ea9d5225f65cb954
JP <jp.moro.machado@student.nhlstenden.com>
2026-01-16 02:14:18 +0100

    fixed css href

--- school_portal/Pages/morningstar-create.php
+++ school_portal/Pages/morningstar-create.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" text="text/css" href="morningstar.css">
+    <link rel="stylesheet" text="text/css" href="morningstar-create.css">
     <title>Document</title>
 </head>
 <body>
